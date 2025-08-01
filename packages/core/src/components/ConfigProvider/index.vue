e69dd88a2f478e9dac221d3546a87ccc502051cf
yuguaa <280722781@qq.com>
2025-08-01 16:06:52 +0800

    refactor: loadCdn is removed

--- packages/core/src/components/ConfigProvider/index.vue
+++ packages/core/src/components/ConfigProvider/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import type { ConfigProviderProps } from './types';
-import { loadCdn } from '../../utils/loadCdn';
 import { APP_CONFIG_PROVIDE_KEY, DEFAULT_APP_CONFIG } from './constants';
 
 const props = withDefaults(defineProps<ConfigProviderProps>(), {});
@@ -8,10 +7,6 @@ const props = withDefaults(defineProps<ConfigProviderProps>(), {});
 provide<ConfigProviderProps>(APP_CONFIG_PROVIDE_KEY, {
   md: props.md ?? DEFAULT_APP_CONFIG.md,
   mdPlugins: props.mdPlugins ?? DEFAULT_APP_CONFIG.mdPlugins
-});
-const cdnAssets = props.cdnAssets ?? DEFAULT_APP_CONFIG.cdnAssets;
-cdnAssets?.forEach(assets => {
-  loadCdn(assets);
 });
 </script>
 
